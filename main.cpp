b83472bda473f9c84d1f94a7baf655ae0becbdb6
meats88 <pcx883mi@gmail.com>
2026-03-24 19:37:58 -0700

    doen

--- main.cpp
+++ main.cpp
@@ -6,18 +6,24 @@ using namespace std;
 
 int main()
 {
-    int rdnum1, rdnum2, rdnum3;
+    int rdnum1, rdnum2, rdnum3, totav;
     int total = 0;
 
     // TODO: seed the random number generator with time(0)
+srand(time(0));
 
     // TODO: generate 3 random numbers between 0 and 99
-
+rdnum1 = rand() % 100;
+rdnum2 = rand() % 100;
+rdnum3 = rand() % 100;
     // TODO: compute the total summation
-
+total = rdnum1 + rdnum2 + rdnum3;
+totav = total / 3;
     // TODO: print the 3 random numbers on the first line
+cout << rdnum1 << " " << rdnum2 << " " << rdnum3 << endl;
+
 
     // TODO: print the total and average (integer division) on the second line
-
+cout << total << " " << totav << endl;
     return 0;
 }
